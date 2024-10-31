6d172a6e08c6794e230b4d943abec26bd0b28c43
Alex <alex.podikoglou@gmail.com>
2024-10-31 16:53:08 +0200

    feat: add nfn_datagen

--- Makefile
+++ Makefile
@@ -2,3 +2,4 @@ FC=gfortran
 
 all:
 	$(FC) src/nfn.f90  -o nfn
+	$(FC) src/nfn_datagen.f90  -o nfn_datagen
---
+++ src/nfn_datagen.f90
@@ -0,0 +1,45 @@
+program nfn_datagen
+  implicit none
+
+  ! ---------------------------------- !
+  ! OPTIONS
+  ! ---------------------------------- !
+
+  ! Dataset Size, in entries
+  integer, parameter :: size = 300
+
+  ! a and c in ax + c
+  real, parameter :: a = 31.3829
+  real, parameter :: c = 7.12
+
+  ! ---------------------------------- !
+  ! ACTUAL VARIABLES
+  ! ---------------------------------- !
+
+  ! Counters
+  integer :: i
+
+  ! Data
+  real :: x, y
+
+  ! initialize rng
+  call RANDOM_INIT(.false., .true.)
+
+  do i=1,size
+    ! generate random x value
+    call RANDOM_NUMBER(x)
+    x = x * 10
+
+    ! calculate y value
+    y = f(x)
+
+    print*, x, y
+  enddo
+
+contains
+  real function f(x_in)
+    real, intent(in) :: x_in
+
+    f = a * x + c
+  end function f
+end program nfn_datagen
